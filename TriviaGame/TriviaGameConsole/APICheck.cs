3c2fbe5f0829d76b6c1f4543c6f55885e8ee91aa
TimMochi <206576152+TimMochi@users.noreply.github.com>
2025-05-07 13:55:11 -0700

    Remove SQLRepo and implement CosmosDB integration
    - Deleted `SQLRepo.cs` file.
    - Replaced `InMemoryRepo` with `CosmosDBRepo` in `APICheck.cs` for user data management.
    - Added `CosmosDBRepo.cs` to handle user interactions with Azure Cosmos DB.

--- TriviaGame/TriviaGameConsole/APICheck.cs
+++ TriviaGame/TriviaGameConsole/APICheck.cs
@@ -3,29 +3,28 @@ using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using TriviaGameApp;
+using User = TriviaGameApp.User;
 
 class Program
 {
     public static async Task Main()
     {
+        CosmosDBRepo datastore = new CosmosDBRepo();
         using HttpClient client = new HttpClient();
         string _url = "https://opentdb.com/api.php?amount=10";
         string json = await client.GetStringAsync(_url);
 
         var result = JsonSerializer.Deserialize<Result>(json);
         var _questionSet = result.results;
-        InMemoryRepo datastore = new InMemoryRepo();
-        User fred = new User("Fred", 0);
-        datastore.Add(fred);
-        
-        var localList = datastore.Get();
-        
-        foreach ( var item in localList )
-        {
-            Console.WriteLine(item.id);
-            
+       // InMemoryRepo datastore = new InMemoryRepo();
+        User currentUser = new User("Fred", 0);
+        datastore.SaveUserAsync(currentUser);
 
-        }
+        var localuser = datastore.Get(currentUser);
+       Console.WriteLine(localuser.Score);
+
+ 
+       
         /*  foreach (var q in _questionSet)
           {
               Console.WriteLine(q.type);
