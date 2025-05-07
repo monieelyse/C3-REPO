e78c45e0ea80f65fa43776b663ac85103d362079
TimMochi <206576152+TimMochi@users.noreply.github.com>
2025-05-07 11:48:43 -0700

    Add user management and Azure Cosmos DB support
    - Updated `TriviaGameApp.csproj` to include `UserSecretsId` and new package references for Azure Cosmos DB.
    - Refactored `APICheck.cs` to implement a `Program` class with an async `Main` method and introduced `InMemoryRepo` for user data management.
    - Updated `TriviaGameConsole.csproj` to reference `Microsoft.Azure.Cosmos`.
    - Created `InMemoryRepo` class for managing users in memory.
    - Introduced `IRepo` interface to define repository methods for user management.
    - Added empty `SQLRepo` class for future SQL implementation.
    - Enhanced `User` class with properties for `id` and `Score`, including a constructor for initialization.

---
+++ TriviaGame/TriviaGameApp/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Platform;
+
+namespace TriviaGameApp
+{
+    public class User
+    {
+        public string id { get; set; }
+        public int Score { get; set; }
+
+       
+    
+
+    public User(string userName, int score)
+        {
+          id = userName;
+            Score = score;
+        }
+    } 
+}
--- TriviaGame/TriviaGameConsole/APICheck.cs
+++ TriviaGame/TriviaGameConsole/APICheck.cs
@@ -4,21 +4,43 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using TriviaGameApp;
 
-
-using HttpClient client = new HttpClient();
-string _url = "https://opentdb.com/api.php?amount=10";
-string json = await client.GetStringAsync(_url);
-
-var result = JsonSerializer.Deserialize<Result>(json);
-var _questionSet = result.results;
-
-
-foreach (var q in _questionSet)
+class Program
 {
-    Console.WriteLine(q.type);
-    Console.WriteLine(q.difficulty);
-    Console.WriteLine(q.question);
-    Console.WriteLine(q.correct_answer);
+    public static async Task Main()
+    {
+        using HttpClient client = new HttpClient();
+        string _url = "https://opentdb.com/api.php?amount=10";
+        string json = await client.GetStringAsync(_url);
+
+        var result = JsonSerializer.Deserialize<Result>(json);
+        var _questionSet = result.results;
+        InMemoryRepo datastore = new InMemoryRepo();
+        User fred = new User("Fred", 0);
+        datastore.Add(fred);
+        
+        var localList = datastore.Get();
+        
+        foreach ( var item in localList )
+        {
+            Console.WriteLine(item.id);
+            
+
+        }
+        /*  foreach (var q in _questionSet)
+          {
+              Console.WriteLine(q.type);
+              Console.WriteLine(q.difficulty);
+              Console.WriteLine(q.question);
+              Console.WriteLine(q.correct_answer);
 
 
+          }
+
+
+          */
+
+       // Console.WriteLine(datastore.Get());
+
+        
+    }
 }
