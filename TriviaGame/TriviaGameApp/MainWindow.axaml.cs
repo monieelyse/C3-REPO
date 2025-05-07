ce4c305d39cc95bbfda329c2fb3029e151b99550
monica elyse <monica.elyse.nelson@outlook.com>
2025-05-07 11:11:41 -0400

    Update MainWindow.axaml.cs
    button click event

--- TriviaGame/TriviaGameApp/MainWindow.axaml.cs
+++ TriviaGame/TriviaGameApp/MainWindow.axaml.cs
@@ -2,11 +2,16 @@ using Avalonia.Controls;
 
 namespace TriviaGameApp
 {
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+    public MainWindow()
+{
+    InitializeComponent();
+    var startButton = this.FindControl<Button>("StartButton"); // Find the button
+    startButton.Click += StartGame; // Attach the click event
+}
+
+private void StartGame(object sender, RoutedEventArgs e)
+{
+    var questionWindow = new QuestionWindow(); // Create next screen
+    questionWindow.Show(); // Open the question screen
+    this.Close(); // Close current window
 }
